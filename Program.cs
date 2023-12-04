d8bf085602f3213d74141ee9e1d4e00996463cd7
juarika <lozadagomezj@gmail.com>
2023-12-04 15:18:12 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ingrese el multiplicador: ");
+        int multiplicador = Int32.Parse(Console.ReadLine());
+        Console.Write("Ingrese el multiplicando: ");
+        int multiplicando = Int32.Parse(Console.ReadLine());
+        int resultado = 0;
+        do
+        {
+            if (multiplicador % 2 != 0)
+            {
+                resultado += multiplicando;
+            }
+            multiplicador /= 2;
+            multiplicando *= 2;
+        } while (multiplicador > 1);
+        resultado += multiplicando;
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+}
